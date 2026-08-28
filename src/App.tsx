4915265d9c9c19e366c9d9dbfec1ce4cb9dd6cd8
Jahnyere Jonathan Ike <jahnyjon2016@gmail.com>
2026-08-28 13:02:08 +0100

    add inital sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
+import Sidebar from "./components/Sidebar"
 
 
 function App() {
 
 
   return (
-    <div className='bg-slate-300 min-h-screen justify-center items-center'>
-      <h1 className="font-bold text-white-50">
-        Nexa Bank
-      </h1>
+    <Sidebar />
 
-    </div>
   )
 }
 
